e5549ddfe3bd725aa6a9b75cabd4b9819c8f5afd
welik carlos <frontendwelikcarlos@gmail.com>
2026-02-08 01:50:40 -0300

    secao projetos feito

--- script.js
+++ script.js
@@ -80,4 +80,56 @@ cards.forEach(card => {
 
 
 
+window.addEventListener("load", function() {
+    
+    const folders = gsap.utils.toArray(".folder-card");
+    const folder2 = document.querySelector(".item2");
+    const folder3 = document.querySelector(".item3");
+    const folder4 = document.querySelector(".item4");
+    const folder5 = document.querySelector(".item5");
+    const folder6 = document.querySelector(".item6");
 
+    // 1. A Timeline do Scroll (Mantém as pastas subindo conforme você desce)
+    const tlPastas = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".folder-section",
+            scroller: pageContainer,
+            start: "top top",
+            end: "+=2000",
+            pin: true,
+            scrub: 1,
+        }
+    });
+
+    tlPastas.to(folder2, { y: "40px", ease: "none" })
+    tlPastas.to(folder3, { y: "80px", ease: "none" });
+    tlPastas.to(folder4, { y: "120px", ease: "none" });
+    tlPastas.to(folder5, { y: "160px", ease: "none" });
+    tlPastas.to(folder6, { y: "200px", ease: "none" });
+
+
+    // 2. AÇÃO DE CLIQUE: Trazer para o topo
+    folders.forEach((folder) => {
+        folder.addEventListener("click", () => {
+            // Se a pasta clicada já estiver no topo, nós resetamos
+            if (folder.classList.contains("active-folder")) {
+                folder.classList.remove("active-folder");
+            } else {
+                // Remove o destaque de qualquer outra pasta
+                folders.forEach(f => f.classList.remove("active-folder"));
+                // Adiciona o destaque na pasta clicada
+                folder.classList.add("active-folder");
+            }
+        });
+    });
+
+    ScrollTrigger.refresh();
+});
+
+
+
+
+
+    
+
+ 
